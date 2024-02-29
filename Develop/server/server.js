a46ae0313d40c8ade5a56800820d8d7e3fc49bef
song-richard <rsong1993@gmail.com>
2024-02-29 14:21:06 -0800

    Updated imports

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -4,9 +4,9 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 //Apollo Server
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require('apollo-server-express');
+const { expressMiddleware } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
 
 //Auth
 const { authMiddleware } = require('./utils/auth');
