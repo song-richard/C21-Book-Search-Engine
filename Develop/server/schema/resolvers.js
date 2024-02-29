0855b2c84e01773ac58a9cec56e18151e573f432
song-richard <rsong1993@gmail.com>
2024-02-29 14:38:57 -0800

    Added initial query for resolvers

--- Develop/server/schema/resolvers.js
+++ Develop/server/schema/resolvers.js
@@ -1,5 +1,25 @@
-const resolvers = {
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+module.exports = {
     Query: {
-        
-    }
+        me: async (parent,  args, { user }) => {
+            try {
+                const foundUser = await User.findOne({
+                    $or: [{ _id: user._id}, { username: user.username }],
+                });
+
+                if (!foundUser) {
+                    return { message: 'Cannot find a user' };
+                }
+
+                return foundUser;
+
+            } catch (error) {
+                console.log(error);
+                return { message: 'Error', error };
+            }
+        }
+
+    },
 }
