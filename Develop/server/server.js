c78cdfedbbfc31b89a2212b2cb2bad7076320476
song-richard <rsong1993@gmail.com>
2024-02-29 14:14:35 -0800

    Invoked connection function

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -34,7 +34,7 @@ const connectToApollo = async () => {
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
-  }
+  };
   
   app.use('/graphql', expressMiddleware(server,{
     context: authMiddleware
@@ -47,3 +47,5 @@ const connectToApollo = async () => {
     });
   });
 };
+
+connectToApollo();
