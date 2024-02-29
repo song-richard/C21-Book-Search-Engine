4a868f08b852f6d8582983ea8ae4101198f19b6e
song-richard <rsong1993@gmail.com>
2024-02-29 14:14:10 -0800

    Added connectToApollo to establish connection with Apollo server. Added connection logic

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -18,18 +18,32 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers
-})
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+//Establish connection with Apollo Server
+const connectToApollo = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+  
+  app.use('/graphql', expressMiddleware(server,{
+    context: authMiddleware
+  }));
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`GraphQL started on: http://localhost:${PORT}/graphql`);
+    });
+  });
+};
